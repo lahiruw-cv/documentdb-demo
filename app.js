bc4ff2c767a3c48452152f2f6b98b9e60fae9f48
lahiruw-cv <104359923+lahiruw-cv@users.noreply.github.com>
2022-08-17 09:24:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+var MongoClient = require('mongodb').MongoClient
 
-app.get("/", (req, res) => {
-  res.send(
-    "<h1>Auto scalling demo app</h1> <h4>Message Success</h4> <p>Version: 1.0.0</p>"
-  );
+//Create a MongoDB client, open a connection to DocDB; as a replica set,
+//  and specify the read preference as secondary preferred
+
+var client = MongoClient.connect(
+'mongodb://<sample-user>:<password>@sample-cluster.node.us-east-1.docdb.amazonaws.com:27017/sample-database?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false',
+{
+  tlsCAFile: `rds-combined-ca-bundle.pem` //Specify the DocDB; cert
+},
+function(err, client) {
+    if(err)
+        throw err;
+
+    //Specify the database to be used
+    db = client.db('sample-database');
+
+    //Specify the collection to be used
+    col = db.collection('sample-collection');
+
+    //Insert a single document
+    col.insertOne({'hello':'Amazon DocumentDB'}, function(err, result){
+      //Find the document that was previously written
+      col.findOne({'hello':'DocDB;'}, function(err, result){
+        //Print the result to the screen
+        console.log(result);
+
+        //Close the connection
+        client.close()
+      });
+   });
 });
-
-app.listen(port, () => {
-  console.log(`Demo app is up and listening to port ${port}`);
-});
-
-var MongoClient = require('mongodb').MongoClient,fs = require('fs');
-var ca = [fs.readFileSync("rds-combined-ca-bundle.pem")];
-var connection_string = "mongodb://administrator:1234567La8@modjoul-rfid-cluster.cluster-cqwjen1x09yo.us-east-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false";
-
-MongoClient.connect(
-        connection_string, {
-            sslValidate: true,
-            sslCA: ca,
-            useNewUrlParser: true
-        },
-        function (err,client) {
-            console.log(err+" , "+ client);
-        });
+                    
